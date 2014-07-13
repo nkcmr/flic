92c3a74dac5e1a66ffc96c917f9462cadbaa3f93
Nick Comer <nick@comer.io>
2014-07-12 21:27:18 -0400

    nice header copyright

--- test/test.js
+++ test/test.js
@@ -1,6 +1,31 @@
+// flic
+// written by: nick comer (http://nick.comer.io)
+
+// Copyright (c) 2014 Nick Comer
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 var flic = require('../lib/flic');
 var Bridge = flic.bridge;
 var Node = flic.node;
+
+var util = require('util');
 
 var test_bridge;
 
@@ -31,7 +56,7 @@ exports['Bridge construct - invalid port number (too high)'] = function(test) {
 exports['Node construct - nominal'] = function(test) {
   test.expect(1);
   var node = new Node('node', function(err) {
-    test.equal(err, null, 'Callback returned an unexpected error.');
+    test.equal(err, null, util.format('Callback returned an unexpected error.', err));
     test.done();
   });
 }
