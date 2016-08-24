8ed447dd59cb207831b39369ac0f7c72fbd3aa24
nick comer <nick@comer.io>
2016-08-23 23:29:51 -0700

    updating to conform to standard js style

--- test/test.js
+++ test/test.js
@@ -12,61 +12,61 @@ var async = require('async')
 
 describe('Bridge', function () {
   describe('construction', function () {
-    var test_bridge
+    var testBridge
 
     it('can still be accessed through flic.bridge', function () {
       assert(Bridge === flic.bridge)
     })
 
     it('should construct normally', function (done) {
-      test_bridge = flic.createBridge(function () {
-        assert(test_bridge instanceof Bridge)
-        assert.equal(test_bridge._config.port, 8221)
-        test_bridge._server.close(done)
+      testBridge = flic.createBridge(function () {
+        assert(testBridge instanceof Bridge)
+        assert.equal(testBridge._config.port, 8221)
+        testBridge._server.close(done)
       })
-      test_bridge._server.unref()
+      testBridge._server.unref()
     })
 
     it('should allow setting of the listening port', function (done) {
-      test_bridge = flic.createBridge({
+      testBridge = flic.createBridge({
         port: 9003
       }, function (err) {
         assert.ifError(err)
-        assert.equal(test_bridge._config.port, 9003)
-        test_bridge._server.close(done)
+        assert.equal(testBridge._config.port, 9003)
+        testBridge._server.close(done)
       })
-      test_bridge._server.unref()
+      testBridge._server.unref()
     })
 
     it('should pass any errors to a listener callback', function (done) {
-      test_bridge = flic.createBridge(function () {
-        var dupe_bridge = flic.createBridge(function (err) {
+      testBridge = flic.createBridge(function () {
+        var dupeBridge = flic.createBridge(function (err) {
           assert(err)
-          test_bridge._server.close(done)
+          testBridge._server.close(done)
         })
-        dupe_bridge._server.unref()
+        dupeBridge._server.unref()
       })
-      test_bridge._server.unref()
+      testBridge._server.unref()
     })
   })
 
   describe('close', function () {
-    var b, good_nodes
+    var b, goodNodes
 
-    good_nodes = []
+    goodNodes = []
 
     before(function (done) {
       done = _.after(2, done)
       b = flic.createBridge()
       b._server.unref()
-      good_nodes.push(flic.createNode({
+      goodNodes.push(flic.createNode({
         id: 'node_1',
         connect_callback: function (err) {
           assert.ifError(err)
           done()
         }
       }))
-      good_nodes.push(flic.createNode({
+      goodNodes.push(flic.createNode({
         id: 'node_2',
         connect_callback: function (err) {
           assert.ifError(err)
@@ -82,14 +82,14 @@ describe('Bridge', function () {
     it('should send a close message to all connected nodes', function (done) {
       async.parallel([
         function (cb) {
-          good_nodes[0].on('close', function (parting_data) {
-            assert.equal(parting_data, 'live long and prosper')
+          goodNodes[0].on('close', function (partingData) {
+            assert.equal(partingData, 'live long and prosper')
             cb()
           })
         },
         function (cb) {
-          good_nodes[1].on('close', function (parting_data) {
-            assert.equal(parting_data, 'live long and prosper')
+          goodNodes[1].on('close', function (partingData) {
+            assert.equal(partingData, 'live long and prosper')
             cb()
           })
         }
@@ -183,22 +183,22 @@ describe('Node', function () {
   })
 
   describe('tell', function () {
-    var b, good_nodes
+    var b, goodNodes
 
-    good_nodes = []
+    goodNodes = []
 
     before(function (done) {
       done = _.after(2, done)
       b = new Bridge()
       b._server.unref()
-      good_nodes.push(flic.createNode({
+      goodNodes.push(flic.createNode({
         id: 'node_1',
         connect_callback: function (err) {
           assert.ifError(err)
           done()
         }
       }))
-      good_nodes.push(flic.createNode({
+      goodNodes.push(flic.createNode({
         id: 'node_2',
         connect_callback: function (err) {
           assert.ifError(err)
@@ -212,25 +212,25 @@ describe('Node', function () {
     })
 
     afterEach(function () {
-      good_nodes.forEach(function (n) {
+      goodNodes.forEach(function (n) {
         n.removeAllListeners()
       })
     })
 
     it('should be able to contact another node', function (done) {
-      good_nodes[0].on('test_event', function (param1) {
+      goodNodes[0].on('test_event', function (param1) {
         assert.equal(param1, 'test_param')
         done()
       })
-      good_nodes[1].tell('node_1:test_event', 'test_param')
+      goodNodes[1].tell('node_1:test_event', 'test_param')
     })
 
     it('should be able to contact another node and send callbacks', function (done) {
-      good_nodes[0].on('test_event', function (param1, ack) {
+      goodNodes[0].on('test_event', function (param1, ack) {
         assert.equal(param1, 'test_param')
         ack('my-reply')
       })
-      good_nodes[1].tell('node_1:test_event', 'test_param', function (a) {
+      goodNodes[1].tell('node_1:test_event', 'test_param', function (a) {
         assert.equal(a, 'my-reply')
         done()
       })
@@ -238,15 +238,15 @@ describe('Node', function () {
 
     it("should fail if the who and what format is not valid ('node_name:remote_event')", function () {
       assert.throws(function () {
-        good_nodes[1].tell('node_1-what-the-hell')
+        goodNodes[1].tell('node_1-what-the-hell')
       })
       assert.throws(function () {
-        good_nodes[1].tell('node_1:too:many:colons')
+        goodNodes[1].tell('node_1:too:many:colons')
       })
     })
 
     it('should fail if the node being told does not exist', function (done) {
-      good_nodes[0].tell('me-no-exist:remote_event', function (err) {
+      goodNodes[0].tell('me-no-exist:remote_event', function (err) {
         assert.equal(err, 'unknown-node')
         done()
       })
@@ -254,21 +254,21 @@ describe('Node', function () {
   })
 
   describe('shout', function () {
-    var b, good_nodes
+    var b, goodNodes
 
-    good_nodes = []
+    goodNodes = []
 
     before(function (done) {
       b = flic.createBridge()
       async.parallel([
         function (cb) {
-          good_nodes.push(flic.createNode({id: 'node_1', connect_callback: cb}))
+          goodNodes.push(flic.createNode({id: 'node_1', connect_callback: cb}))
         },
         function (cb) {
-          good_nodes.push(flic.createNode({id: 'node_2', connect_callback: cb}))
+          goodNodes.push(flic.createNode({id: 'node_2', connect_callback: cb}))
         },
         function (cb) {
-          good_nodes.push(flic.createNode({id: 'node_3', connect_callback: cb}))
+          goodNodes.push(flic.createNode({id: 'node_3', connect_callback: cb}))
         }
       ], function (err) {
         assert.ifError(err)
@@ -281,7 +281,7 @@ describe('Node', function () {
     })
 
     afterEach(function () {
-      good_nodes.forEach(function (n) {
+      goodNodes.forEach(function (n) {
         n.removeAllListeners()
       })
     })
@@ -289,20 +289,20 @@ describe('Node', function () {
     it('should send events to all connected nodes', function (done) {
       async.parallel([
         function (done) {
-          good_nodes[0].on('shout_eve', function (param1) {
+          goodNodes[0].on('shout_eve', function (param1) {
             assert.equal(param1, 'test-param')
             done()
           })
         },
         function (done) {
-          good_nodes[1].on('shout_eve', function (param1) {
+          goodNodes[1].on('shout_eve', function (param1) {
             assert.equal(param1, 'test-param')
             done()
           })
         }
       ], done)
 
-      good_nodes[2].shout('shout_eve', 'test-param')
+      goodNodes[2].shout('shout_eve', 'test-param')
     })
   })
 
